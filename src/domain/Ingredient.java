7931d5945a93cb203e5dd877553f3d76cb19b52a
Max Eriksson <max.eriksson2@iths.se>
2024-10-09 14:05:14 +0200

    test(Ingredient): given constructor args validity
    Test the `name` field of `Ingredient` is all lowercase.
    Test the `name` field of `Ingredient` cannot be empty.
    Test the `name` field of `Ingredient` cannot be white space only.
    Test the `quantity` field of `Ingredient` cannot be less than or
    equal to zero.
    
    NOTE: failing all tests.

---
+++ src/domain/Ingredient.java
@@ -0,0 +1,27 @@
+package domain;
+
+/** Ingredient */
+public class Ingredient {
+
+    private String name;
+    private double quantity;
+    private MeasurementUnit measurementUnit;
+
+    public Ingredient(String name, double quantity, MeasurementUnit measurementUnit) {
+        this.name = name;
+        this.quantity = quantity;
+        this.measurementUnit = measurementUnit;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getQuantity() {
+        return this.quantity;
+    }
+
+    public MeasurementUnit getMeasurementUnit() {
+        return this.measurementUnit;
+    }
+}
---
+++ src/domain/MeasurementUnit.java
@@ -0,0 +1,14 @@
+package domain;
+
+/** MeasurementUnit */
+public enum MeasurementUnit {
+    DECILITRES("decilitres"),
+    GRAMS("grams"),
+    PIECES("pieces");
+
+    public final String value;
+
+    private MeasurementUnit(String value) {
+        this.value = value;
+    }
+}

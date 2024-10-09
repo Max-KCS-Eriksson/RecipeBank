c7bc88c174c6a94db796d0e7dc77f0b2610df34d
Max Eriksson <max.eriksson2@iths.se>
2024-10-09 14:56:30 +0200

    fix(Ingredient): `quantity` cannot be <= 0
    NOTE: Passing all tests.

--- src/domain/Ingredient.java
+++ src/domain/Ingredient.java
@@ -11,6 +11,9 @@ public class Ingredient {
         if (name.isBlank()) {
             throw new IllegalArgumentException("Ingredient name cannot be empty");
         }
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Ingredient quantity must be greatar than 0");
+        }
         this.name = name.toLowerCase();
         this.quantity = quantity;
         this.measurementUnit = measurementUnit;

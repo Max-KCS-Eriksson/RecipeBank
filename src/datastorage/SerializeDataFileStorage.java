affca66161c303f4a915cd71a042f0da1a504638
Max Eriksson <max.eriksson2@iths.se>
2024-10-11 19:26:34 +0200

    feat(datastorage): deserialize - one step closer
    Test throw `ClassCastException` with below traceback:
    java.lang.ClassCastException: class domain.Recipe cannot be cast to class java.util.Collection
    
    The problem seems to be related to the `write` method and not the `read`
    method. Further investigation will be done, and left a "TODO:" comment
    to indicate place for possible bug.
    
    NOTE: Not passing tests - see "TODO:" comment for leads

--- src/datastorage/SerializeDataFileStorage.java
+++ src/datastorage/SerializeDataFileStorage.java
@@ -1,8 +1,11 @@
 package datastorage;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.UncheckedIOException;
 import java.util.ArrayList;
@@ -20,6 +23,7 @@ public class SerializeDataFileStorage {
     public <T> void write(Collection<T> objects) {
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(FILE))) {
             for (T object : objects) {
+                // TODO: this might be the problem
                 out.writeObject(object);
             }
         } catch (IOException e) {
@@ -27,7 +31,18 @@ public class SerializeDataFileStorage {
         }
     }
 
+    @SuppressWarnings("unchecked")
     public <T> Collection<T> read() {
-        return new ArrayList<T>(); // TODO: Implement
+        Collection<T> objects;
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(FILE))) {
+            objects = (Collection<T>) in.readObject();
+        } catch (FileNotFoundException e) {
+            objects = new ArrayList<>();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e); // Fatal error, but reduce boilerplate with unchecked
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e); // Fatal error, but reduce boilerplate with unchecked
+        }
+        return objects;
     }
 }

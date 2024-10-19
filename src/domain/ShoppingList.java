364d1070fe524bcc611b7bf21fadfebdac3ec4ab
Max Eriksson <max.eriksson2@iths.se>
2024-10-19 18:05:28 +0200

    feat(domain): class representing ingredients
    Main responsibility is to represent `List<Ingredient>` as a string.

---
+++ src/domain/ShoppingList.java
@@ -0,0 +1,26 @@
+package domain;
+
+import java.util.List;
+
+/** ShoppingList */
+public class ShoppingList extends NamedObject {
+
+    private List<Ingredient> ingredients;
+
+    public ShoppingList(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Shopping List\n");
+        for (int i = 0; i < ingredients.size(); i++) {
+            builder.append((i + 1) + ")\t" + ingredients.get(i));
+            if (i != ingredients.size() - 1) {
+                builder.append("\n");
+            }
+        }
+        return builder.toString();
+    }
+}
